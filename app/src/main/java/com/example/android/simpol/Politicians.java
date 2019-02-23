86a01ce18c9e0dab73bdc36d528dc7a1a13ba20d
Annie <liua1@bxscience.edu>
2019-02-22 23:49:37 -0500

    Politicians

--- app/src/main/java/com/example/android/simpol/Politicians.java
+++ app/src/main/java/com/example/android/simpol/Politicians.java
@@ -2,10 +2,16 @@ package com.example.android.simpol;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,14 +22,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class Politicians extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     private OnFragmentInteractionListener mListener;
 
@@ -31,38 +31,71 @@ public class Politicians extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Politicians.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Politicians newInstance(String param1, String param2) {
         Politicians fragment = new Politicians();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_politicians, container, false);
+        View view = inflater.inflate(R.layout.fragment_politicians, container, false);
+        tabLayout = view.findViewById(R.id.tabLayout);
+        viewPager = view.findViewById(R.id.viewPager);
+
+        return view;
+    }
+
+    private void setUpTabLayout() {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getFragmentManager());
+        adapter.add(new YourFragment1(), "Home");
+        adapter.add(new YourFragment2(), "Trending");
+        adapter.add(new YourFragment3(), "Friends");
+        viewPager.setAdapter(adapter);
+        viewPager.setPageMargin((int) getResources().getDimension(R.dimen.view_pager_gap));
+        viewPager.setPageMarginDrawable(R.color.background);
+        viewPager.setOffscreenPageLimit(2);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private List<Fragment> fragmentList = new ArrayList<>();
+        private List<String> titleList = new ArrayList<>();
+
+        private ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        void add(Fragment fragment, String title) {
+            fragmentList.add(fragment);
+            titleList.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titleList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        @Override
+        public int getItemPosition(Object object) {
+            return POSITION_NONE;
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
